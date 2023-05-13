cffa2c2081daf9b1cc9ddf7b822c32193ff171dd
Omar_Ameer <omar.ameer244@gmail.com>
2023-05-13 22:05:47 +0300

    1st Commit

---
+++ android/app/src/main/kotlin/com/example/for_eve/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.for_eve
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ForEve extends StatelessWidget {
+  const ForEve({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
